7de3f204ec1fcb961f627d5f8478fa4ad3aee2f7
pworre <paulew@stud.ntnu.no>
2025-10-07 12:59:31 +0200

    Test script DCT in main for SPI

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
 #include "drivers/oled.h"
 #include "drivers/fonts.h"
 #include "drivers/menu.h"
+#include <util/delay.h>
 
 #define TEST_PIN 1
 
@@ -30,6 +31,29 @@ int main(){
 
     SPI_MasterInit();
     oled_init();
+
+    while(1){
+        uint8_t buttons;
+        // Buttons consists of 3 bytes: RIGHT, LEFT, NAV
+        SPI_MasterTransmit((0x04), IO_BOARD);
+        for (int byte_nr = 0; byte_nr < 3; byte_nr++){
+            switch(byte_nr){
+                case 0: // RIGHT
+                    buttons = SPI_read(0, IO_BOARD);
+                case 1: // LEFT
+                    buttons = SPI_read(0, IO_BOARD);
+                case 2: // NAV
+                    buttons = SPI_read(0, IO_BOARD);
+                    if ((buttons & (1 << 4)) == 1){
+                        printf("NAV-knapp trykket");
+                    }
+            }
+        _delay_us(30);
+        SPI_MasterTransmit((0x05), IO_BOARD);
+        SPI_MasterTransmit(3, IO_BOARD);
+        SPI_MasterTransmit(1);
+    }
+    }
     
     
     /*char* text = "NEW GAME";
@@ -44,7 +68,7 @@ int main(){
     char* text0 = "TEST";
     oled_write_string(text0, 8, 3, 30);
     oled_draw(DIK, 5, 80);*/
-    main_menu();
+    //main_menu();
 
     //oled_fill_screen();
     
@@ -53,10 +77,10 @@ int main(){
 
     //oled_goto_line(2);
     //transmit_oled_data(1);
-    while(1){
+    //while(1){
         //printf("%d", get_adc_data(1)); 
         //ADC_print();
         //oled_fill_screen();
-    }
+    //}
     return 0;
 }
