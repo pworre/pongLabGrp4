76d73272fb0207b97e15be19a0df7ac8d3ba7c37
pworre <paulew@stud.ntnu.no>
2025-10-28 16:11:25 +0100

    funket ikke igjen

--- node_2/drivers/can_controller.c
+++ node_2/drivers/can_controller.c
@@ -58,7 +58,7 @@ uint8_t can_init(uint32_t can_br, uint8_t num_tx_mb, uint8_t num_rx_mb)
 {
 	
 	//Make sure num_rx_mb and num_tx_mb is valid
-	if(num_rx_mb > 8 | num_tx_mb > 8 | num_rx_mb + num_tx_mb > 8)
+	if((num_rx_mb > 8) || (num_tx_mb > 8) || (num_rx_mb + num_tx_mb > 8))
 	{
 		return 1; //Too many mailboxes is configured
 	}
@@ -111,19 +111,6 @@ uint8_t can_init(uint32_t can_br, uint8_t num_tx_mb, uint8_t num_rx_mb)
 
 	uint32_t can_ier = 0;
 
-	/* Configure receive mailboxes */
-	for (int n = num_tx_mb; n <= num_rx_mb + num_tx_mb; n++)  //Simply one mailbox setup for all messages. You might want to apply filter for them.
-	{
-		CAN0->CAN_MB[n].CAN_MAM = 0; //Accept all messages
-		CAN0->CAN_MB[n].CAN_MID = CAN_MID_MIDE;
-		//CAN0->CAN_MB[n].CAN_MID &= ~(1 << 29); // Disable extended identifier
-		//CAN0->CAN_MB[n].CAN_MID = 0;
-		CAN0->CAN_MB[n].CAN_MMR = (CAN_MMR_MOT_MB_RX);
-		CAN0->CAN_MB[n].CAN_MCR = CAN_MCR_MTCR;		// |= CAN_MCR_MTCR;
-
-		can_ier |= 1 << n; //Enable interrupt on rx mailbox
-	}
-	
 	/*Configure transmit mailboxes */
 	for (int n = 0; n < num_tx_mb; n++)
 	{
@@ -135,6 +122,21 @@ uint8_t can_init(uint32_t can_br, uint8_t num_tx_mb, uint8_t num_rx_mb)
     	CAN0->CAN_MB[n].CAN_MCR = CAN_MCR_MTCR;
 
 	}
+
+	/* Configure receive mailboxes */
+	for (int n = num_tx_mb; n < (num_rx_mb + num_tx_mb); n++)  //Simply one mailbox setup for all messages. You might want to apply filter for them.
+	{
+		CAN0->CAN_MB[n].CAN_MAM = 0; //Accept all messages
+		CAN0->CAN_MB[n].CAN_MID = CAN_MID_MIDE;
+		//CAN0->CAN_MB[n].CAN_MID &= ~(1 << 29); // Disable extended identifier
+		//CAN0->CAN_MB[n].CAN_MID = 0;
+		CAN0->CAN_MB[n].CAN_MMR = (CAN_MMR_MOT_MB_RX);
+		CAN0->CAN_MB[n].CAN_MCR = CAN_MCR_MTCR;		// |= CAN_MCR_MTCR;
+
+		can_ier |= 1 << n; //Enable interrupt on rx mailbox
+	}
+	
+	
 	
 	/****** End of mailbox configuraion ******/
 
@@ -146,6 +148,10 @@ uint8_t can_init(uint32_t can_br, uint8_t num_tx_mb, uint8_t num_rx_mb)
 	NVIC_EnableIRQ(ID_CAN0);
 
 	CAN0->CAN_WPMR |= 1;
+
+	for (volatile uint8_t time = 0; time < 100; time++){
+		__asm__("nop");
+	}
 
 	//enable CAN
 	CAN0->CAN_MR |= CAN_MR_CANEN;
@@ -166,7 +172,7 @@ uint8_t can_init(uint32_t can_br, uint8_t num_tx_mb, uint8_t num_rx_mb)
 uint8_t can_send(CAN_MESSAGE* can_msg, uint8_t tx_mb_id)
 {
 	//Check that mailbox is ready
-	if((CAN0->CAN_MB[tx_mb_id].CAN_MSR & CAN_MSR_MRDY != 0))
+	if((CAN0->CAN_MB[tx_mb_id].CAN_MSR & CAN_MSR_MRDY) != 0)
 	{
 		//Set message ID and use CAN 2.0B protocol
 		CAN0->CAN_MB[tx_mb_id].CAN_MID = CAN_MID_MIDvA(can_msg->id) | CAN_MID_MIDE; // CAN_MID_MIDE ;
--- node_2/main.c
+++ node_2/main.c
@@ -33,9 +33,10 @@ int main()
 
     //uint32_t can_br = 0x001c0008;
     uint32_t can_br = 500000;
-    uint8_t num_tx_mb = 1;
+    uint8_t num_tx_mb = 2;
     uint8_t num_rx_mb = 2;
-    can_init_def_tx_rx_mb(can_br);
+    //can_init_def_tx_rx_mb(can_br);
+    can_init(can_br, num_tx_mb, num_rx_mb);
     __enable_irq();
 
     uint32_t primask = __get_PRIMASK();
@@ -54,6 +55,11 @@ int main()
 
     while (1)
     {
+        // NESTE GANG ! ! ! ! !
+        // TEST NODE 2 LOOPBACKMODE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        // CAN0->CAN_MR |= CAN_MR_LPM;  // Loopback Mode
+
+
         /*
         // TEST
         PIOB->PIO_PER |= PIO_PB13;
@@ -67,16 +73,19 @@ int main()
         printf("can_receive1: %d\r\n", a);
         */
 
-        uint32_t msr = CAN0->CAN_MB[tx_mb_id].CAN_MSR;
-        uint32_t mmr = CAN0->CAN_MB[tx_mb_id].CAN_MMR;
+        uint32_t msr = CAN0->CAN_MB[0].CAN_MSR;
+        uint32_t mmr = CAN0->CAN_MB[0].CAN_MMR;
         uint32_t mot = (mmr & CAN_MMR_MOT_Msk) >> CAN_MMR_MOT_Pos;
         uint32_t sr  = CAN0->CAN_SR;
 
         printf("TX_MB[%d]: MSR=0x%08lX MMR=0x%08lX MOT=%lu SR=0x%08lX\n\r", 
-                tx_mb_id, msr, mmr, mot, sr);
+                0, msr, mmr, mot, sr);
 
+        CAN0->CAN_MB[1].CAN_MCR = CAN_MCR_MTCR;
 
-        if (can_send(&msg, 0) == 1){
+        can_send(&msg, 1);
+        
+        if (can_send(&msg, 1) == 1){
             printf("ikke sender\r\n");
         } else {
             printf("SENDER :)");
